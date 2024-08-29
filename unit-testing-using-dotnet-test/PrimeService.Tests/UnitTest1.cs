b4ca4c5736e8104dc9e2fd49fbb25649f01ad96d
Aleksandra Bruska <s185454@student.pg.edu.pl>
2024-08-29 10:22:46 +0200

    Group work

--- unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
+++ unit-testing-using-dotnet-test/PrimeService.Tests/UnitTest1.cs
@@ -1,10 +1,37 @@
 namespace PrimeService.Tests;
 
+using System.ComponentModel.DataAnnotations;
+using PrimeService;
+
 public class UnitTest1
 {
+   
     [Fact]
-    public void Test1()
+    public void CheckWithdraw()
     {
+        var myAccount = new BankAccount();
+        int currSaldo = myAccount.GetSaldo();
+        myAccount.Withdraw(100);
+        int newSaldo = myAccount.GetSaldo();
+        Assert.Equal(100, currSaldo - newSaldo);
+    }
 
+    [Fact]
+    public void CheckWithdrawLimit()
+    {
+        var myAccount = new BankAccount();
+        int currSaldo = myAccount.GetSaldo();
+        bool success = myAccount.Withdraw(currSaldo + 1000);
+        Assert.Equal(false, success);
+
+    }
+    [Fact]
+    public void CheckDeposit()
+    {
+        var myAccount = new BankAccount();
+        int currSaldo = myAccount.GetSaldo();
+        myAccount.Deposit(100);
+        int newSaldo = myAccount.GetSaldo();
+        Assert.Equal(100+currSaldo, newSaldo);
     }
 }
---
+++ unit-testing-using-dotnet-test/PrimeService/BankAccount.cs
@@ -0,0 +1,27 @@
+﻿namespace PrimeService;
+
+public class BankAccount
+{
+    private int saldo;
+    public BankAccount()
+    {
+        this.saldo = 1000;
+    }
+    public bool Withdraw(int amount)
+    {
+        if (amount > saldo)
+            return false;
+        this.saldo -= amount;
+        return true;
+       
+    }
+    public int GetSaldo()
+    {
+        return this.saldo;
+    }
+
+    public void Deposit(int amount)
+    {
+        this.saldo = amount + this.saldo;
+    }
+}
